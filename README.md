d95d8e68a0e3f612609af899c89aca2f4ef4f37b
Asimo <rvolenc@gmail.com>
2025-01-16 14:40:04 +0400

    feat: implement template, add header

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# English-learning-app
+# ArtLingua
---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="logo.svg" height={80} width={80} alt="logo"></Image>
+          <h1 className="text-2xl font-extrabold text-blue-600 tracking-wide">
+            ArtLingua
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin"></Loader>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/"></UserButton>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              signUpFallbackRedirectUrl="/learn"
+              forceRedirectUrl="/learn"
+            >
+              <Button size="lg" variant="ghost">
+                Log in
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
+    </header>
+  );
+}
